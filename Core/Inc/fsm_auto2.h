4b9978f0b3b80fff2216941e2216538eb0d71313
ThinhDang <danggiathinhgocong@gmail.com>
2022-11-15 22:44:37 +0700

    Add increase-decrease for 3 led light
    Add code for displaying modes

---
+++ Core/Inc/fsm_auto2.h
@@ -0,0 +1,15 @@
+/*
+ * fsm_automatic_2.h
+ *
+ *  Created on: Oct 29, 2022
+ *      Author: phucd
+ */
+
+#ifndef INC_FSM_AUTO2_H_
+#define INC_FSM_AUTO2_H_
+
+#include "global.h"
+
+void fsm_auto_run_2();
+
+#endif /* INC_FSM_AUTO2_H_ */
---
+++ Core/Inc/fsm_set.h
@@ -0,0 +1,15 @@
+/*
+ * fsm_manual.h
+ *
+ *  Created on: Oct 7, 2022
+ *      Author: phucd
+ */
+
+#ifndef INC_FSM_SET_H_
+#define INC_FSM_SET_H_
+
+#include "global.h"
+
+void fsm_set_run();
+
+#endif /* INC_FSM_SET_H_ */
